2a713a546806b1152342092c21bc3c0b8f54e48d
grbeno <grbeno@gmail.com>
2023-12-05 06:38:52 +0100

    bg-img

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-start-2023.up.railway.app' ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-start-2023.up.railway.app', 'django-start-2023.lm.r.appspot.com' ]
 
 
 # Application definition
